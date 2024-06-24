6a61bdf1366e9914376453ed297814260160a5dd
viltsevdanila <viltsevdanila@Noutbuk-Denis-Viltsev.local>
2024-06-25 02:43:35 +0400

    homework_3. widget refactoring

--- WBApp/WBWidget/WBWidget.swift
+++ WBApp/WBWidget/WBWidget.swift
@@ -13,7 +13,7 @@ struct WBWidget: Widget {
     
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
-            ActiveUsersWidgetEntryView(entry: entry)
+            WBWidgetEntryView(entry: entry)
                 .containerBackground(.fill.tertiary, for: .widget)
         }
         .configurationDisplayName("Online users")
@@ -50,79 +50,4 @@ struct Provider: TimelineProvider {
 struct SimpleEntry: TimelineEntry {
     let date: Date
     let users: [WidgetContact]
-}
-
-struct ActiveUsersWidgetEntryView : View {
-    @State var entry: Provider.Entry
-    
-    var body: some View {
-        VStack(alignment: .leading) {
-            ForEach(entry.users) { user in
-                HStack {
-                    if let image = user.image {
-                        Image(image)
-                            .resizable()
-                            .frame(width: 30, height: 30)
-                            .cornerRadius(15)
-                    } else {
-                        RoundedRectangle(cornerRadius: 14)
-                            .fill(Color.pink)
-                            .frame(width: 48, height: 48)
-                            .overlay {
-                                Text(WidgetContact.extractInitials(from: user.name))
-                                    .foregroundStyle(.white)
-                            }
-                    }
-                    Text(user.name)
-                        .font(.headline)
-                        .padding()
-                }
-            }
-        }
-        .padding()
-    }
-}
-
-struct WidgetContact: Identifiable, Hashable {
-    let id = UUID()
-    var name: String
-    var image: String?
-    var isOnline: Bool
-    var hasStory: Bool
-    var phone: String
-    var lastSeen: Date = .init()
-}
-
-extension WidgetContact {
-    static let mockContacts: [WidgetContact] = [
-        .init(name: "Danila Viltsev", isOnline: true, hasStory: true, phone: "+ 7 999 999 99 99"),
-        .init(name: "Ivan Ivanov", isOnline: false, hasStory: false, phone: "+ 7 999 999 99 99"),
-        .init(name: "Petr Petrov", isOnline: false, hasStory: false, phone: "+ 7 999 999 99 99"),
-        .init(name: "User Userovich", isOnline: true, hasStory: true, phone: "+ 7 999 999 99 99")
-    ]
-    
-    static func filterContacts(_ contact: String) -> [WidgetContact] {
-        if contact.isEmpty {
-            return mockContacts
-        } else {
-            return mockContacts.filter { $0.name.lowercased().contains(contact.lowercased()) }
-        }
-    }
-    
-    static func extractInitials(from name: String) -> String {
-        let words = name.split(separator: " ")
-        var initials = ""
-        
-        for word in words {
-            if let firstLetter = word.first {
-                initials.append(firstLetter)
-            }
-        }
-        
-        return String(initials)
-    }
-    
-    static func loadUsers() -> [WidgetContact] {
-        WidgetContact.mockContacts.filter { $0.isOnline }
-    }
 }

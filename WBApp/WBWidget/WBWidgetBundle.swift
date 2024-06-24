11af5d06bde4f66937f01fe8b85864bfa9f68dcf
Viltsev <94001621+Viltsev@users.noreply.github.com>
2024-06-24 22:48:28 +0400

    homework_3. widgets

---
+++ WBApp/WBWidget/WBWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  WBWidgetBundle.swift
+//  WBWidget
+//
+//  Created by Данила on 24.06.2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WBWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        WBWidget()
+        WBWidgetLiveActivity()
+    }
+}
